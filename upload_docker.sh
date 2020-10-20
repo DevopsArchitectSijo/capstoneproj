a54a5f384d42377de3d89bedf30125a90c2691dd
DevopsArchitectSijo <53095503+DevopsArchitectSijo@users.noreply.github.com>
2020-10-20 23:48:54 +0530

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,12 +6,12 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-export dockerpath=sijodevops/udacityprojects
+dockerpath=sijodevops/udacityprojects
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login --username sijodevops
-docker tag projectcapstone $dockerpath
+docker tag projectcapstone123 $dockerpath
 # Step 3:
 # Push image to a docker repository
 docker push $dockerpath
